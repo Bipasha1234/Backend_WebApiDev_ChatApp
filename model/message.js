0fb0d1fd12e7c09b8d1a63ed589fb72a4688fd1f
bipasha <bipashalamsal@gmail.com>
2025-01-02 19:08:04 +0545

    socket.io installed and in /model -/chat.js and /message.js is created

---
+++ model/message.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+// Message Schema
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    enum: ['sent', 'delivered', 'read'],
+    default: 'sent',
+  },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
